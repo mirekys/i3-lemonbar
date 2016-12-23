8e9445a7215d5e483f04194d4d338d574185d14f
Jesse Roberts <jesse@clearwateranalytics.com>
2016-12-23 07:13:22 -0700

    remove hacky method of choosing python command

--- i3_lemonbar.py
+++ i3_lemonbar.py
@@ -1,20 +1,11 @@
 #!/usr/bin/env python3
 
 import os
-import platform
 from i3_lemonbar_conf import *
 
-python3_command_map = {'ubuntu': 'python3',
-                       'arch': 'python',
-                       'default': 'python3'}
-try:
-    distribution = platform.linux_distribution()[0].lower()
-    python_command = python3_command_map[distribution]
-except (KeyError, IndexError, TypeError):
-    python_command = python3_command_map['default']
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 lemon = "lemonbar -p -f '%s' -f '%s' -g '%s' -B '%s' -F '%s'" % (font, iconfont, geometry, color_back, color_fore)
-feed = "{python_cmd} -c 'import i3_lemonbar_feeder; i3_lemonbar_feeder.run()'".format(python_cmd=python_command)
+feed = "python3 -c 'import i3_lemonbar_feeder; i3_lemonbar_feeder.run()'"
 
 check_output('cd %s; %s | %s' % (cwd, feed, lemon), shell=True)
